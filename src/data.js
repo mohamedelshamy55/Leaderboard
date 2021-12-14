841f7e0cdd2ce20f184cf014d9934bad3820ef61
mohamedelshamy55 <39459774+mohamedelshamy55@users.noreply.github.com>
2021-12-14 12:19:52 +0200

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,19 +1,19 @@
 const data = [{
-        user: 'Mohamed',
-        score: 100,
-    },
-    {
-        user: 'Ali',
-        score: 80,
-    },
-    {
-        user: 'Mostafa',
-        score: 200,
-    },
-    {
-        user: 'IB',
-        score: 150,
-    },
+  user: 'Mohamed',
+  score: 100,
+},
+{
+  user: 'Ali',
+  score: 80,
+},
+{
+  user: 'Mostafa',
+  score: 200,
+},
+{
+  user: 'IB',
+  score: 150,
+},
 ];
 
 export default data;
